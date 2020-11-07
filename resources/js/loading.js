a682398b3fea5480226a13f274052d521d3b88b2
Henrique Dos Santos Martins <henrique0126@gmail.com>
2020-11-07 18:49:23 +0100

    update

--- resources/js/loading.js
+++ resources/js/loading.js
@@ -1,3 +1,11 @@
 window.addEventListener("load", function() {
   document.querySelector('#loading').style.display = 'none'
 });
+
+const menuList = document.querySelectorAll('#menu .list a');
+
+menuList.forEach(function (item) {
+  item.addEventListener('click', function () {
+    document.querySelector('#loading').style.display = 'block'
+  })
+})
