1d77d0308070ee93cf7d5ea02372b755ba431e9b
Henrique Dos Santos Martins <henrique0126@gmail.com>
2020-12-09 15:08:05 +0100

    update

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,10 @@
 const covers = document.querySelectorAll('#main .cover')
 const coversSize = covers.length
 
-if (( window.innerWidth > 800 ) && ( window.innerHeight > 600 )) {
+// if (( window.innerWidth > 800 ) && ( window.innerHeight > 600 )) {
   window.setInterval(function () {
     const time = new Date().getTime()
     covers[(time - 1) % coversSize].classList.remove('active')
     covers[(time) % coversSize].classList.add('active')
   }, 4999)
-}
+// }
